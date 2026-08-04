6a82212c2659cab17f1a82894bfbe82a3b5a78fe
Samuel maina <murigisamuel54@.com>
2026-08-04 12:04:12 -0600

    Add Javascript functions and company inroduction

---
+++ script.js
@@ -0,0 +1,5 @@
+function introduceCompany(companyName, service){
+    console.log(`Welcome, to ${companyName} where we integrate ${service} to help you solve real life problems`  );
+}
+
+introduceCompany("SM solutions"," technical and business ideas");
