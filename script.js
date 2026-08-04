35c732b3bc1ed6e95d7e5b0fe1b3ae83abd4e9d9
Samuel maina <murigisamuel54@.com>
2026-08-04 13:28:57 -0600

    add javascript interavtive buttons

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function introduceCompany(companyName, service){
-    console.log(`Welcome, to ${companyName} where we integrate ${service} to help you solve real life problems`  );
+    console.log(`Welcome, to ${companyName} where we integrate ${service} to help businesses solve real-life problems.`);
 }
 
 introduceCompany("SM solutions"," technical and business ideas");
+
+const helloBtn = document.getElementById("helloBtn");
+const myWork = document.querySelector("#myWork");
+
+console.log(myWork);
+
+console.log(helloBtn);
+
+helloBtn.addEventListener("click", function () {
+    console.log("The button was clicked!");
+});
+
+myWork.addEventListener("click", function() {
+    console.log("welcome to my projects!");
+});
