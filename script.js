a3b0f2459b8c895561c04e9fd1fb2bdaad12b240
Samuel maina <murigisamuel54@.com>
2026-08-13 16:26:52 -0600

    updates on the web page appearance and functionality

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const budgetBtn = document.getElementById("budgetBtn");
 const budgetMessage = document.getElementById("budgetMessage");
 const projectDecision = document.getElementById("projectDecision");
 const complexityInput = document.getElementById("complexityInput");
+const projectsSection = document.getElementById("projects");
 
 
 
@@ -50,18 +51,64 @@ function checkBudget(budget){
         return "Client needs a larger budget";
     }
 }
-const projectSection = document.getElementById("projects");
+const projectGrid = document.getElementById("projectGrid");
 const projects = [
-    "Portfolio Website", 
-    "Project Evaluator", 
-    "SM solutions"
-];
+    {
+        name: "PORTFOLIO WEBSITE",
+        description: "My personal software engineering portfolio",
+        technology: "HTML, CSS, JavaScript"
+    },
 
-for(let i =0; i< projects.length; i++){
-    const project = document.createElement("p");
-    project.textContent = projects[i];
-    projectSection.appendChild(project);
+    {
+        name: "PROJECT EVALUATOR",
+        description: "A tool that evaluates projects based on budget and complexity",
+        technology: "JavaScript"
+    },
+
+    {
+        name: "SM SOLUTIONS - official",
+        description: "Practical technology solutions for real-world problems ",
+        technology: "HTML, CSS, JavaScript"
+    },
+
+    {
+        name: "SALES DASHBOARD",
+        description: "A Power BI dashboard analyzing business sales data",
+        technology: "Power BI"
+    },
+
+   // {
+
+        //name: "SUPPLY CHAIN AND PROJECT MANAGEMENT",
+       // description: "A tool model for quality control, offshoring etc",
+        //technology: "to be determined"
+
+   // }
+
+];
+for(let i = 0; i < projects.length; i++) {
+    const projectName = document.createElement("h3");
+    const projectDescription = document.createElement("p");
+    const technologyTitle = document.createElement("h4");
+    const projectTechnology = document.createElement("p");
+
+    const projectCard = document.createElement("div")
+    projectCard.classList.add("project-card");
+
+
+    projectName.textContent = projects[i].name;
+    projectDescription.textContent = projects[i].description;
+    technologyTitle.textContent = "Technologies:"
+    projectTechnology.textContent = projects[i].technology
+
+    projectCard.appendChild(projectName);
+    projectCard.appendChild(projectDescription);
+    projectCard.appendChild(technologyTitle);
+    projectCard.appendChild(projectTechnology);
+
+    projectGrid.appendChild(projectCard);
 }
+
 
 
 function evaluateProject(budget, complexity) {
@@ -129,20 +176,22 @@ helloBtn.addEventListener("click", function () {
 myWork.addEventListener("click", function () {
     myWork.textContent = "Projects Loaded!";
     myWork.classList.add("highlight");
-    workMessage.textContent = "Hope you enjoy checking out my work";
 });
 
 
 // About button
-aboutBtn.addEventListener("click", function () {
-    about.classList.toggle("show");
-    if (about.classList.contains("show")) {
-        aboutBtn.textContent = "Hide About Me";
-    } else {
-        aboutBtn.textContent = "More About Me";
-    }
 
-});
+//aboutBtn.addEventListener("click", function () {
+
+    //about.classList.toggle("show");
+
+    //if (about.classList.contains("show")) {
+        //aboutBtn.textContent = "Hide About Me";
+    //} else {
+       // aboutBtn.textContent = "More About Me";
+   // }
+
+//});
 
 //CONTACT ME BUTTOPM
 contactMe.addEventListener("click", function (){
@@ -168,4 +217,16 @@ console.log("button clicked");
     projectDecision.textContent = decision;
     
 
+});
+
+myWork.addEventListener("click", function () {
+    myWork.textContent = "Projects Loaded!";
+
+    projectsSection.scrollIntoView({
+        behavior: "smooth"
+    });
+
+    setTimeout(function () {
+        myWork.textContent = "My Work";
+    }, 500);
 });
